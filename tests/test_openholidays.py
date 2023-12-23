82f662d027868822b93abc2e02ea7f1e12885ff6
Pieter Smets <mail@pietersmets.be>
2023-12-23 14:58:17 +0100

    pytest

--- tests/test_openholidays.py
+++ tests/test_openholidays.py
@@ -11,48 +11,61 @@ endDate = date.today().strftime("%Y-01-10")
 
 oh = OpenHolidays(countryIsoCode, languageIsoCode, subdivisionCode)
 
+
 def test_countryIsoCode():
     assert oh.countryIsoCode == countryIsoCode
+
 
 def test_languageIsoCode():
     assert oh.languageIsoCode == languageIsoCode
 
+
 def test_subdivisionCode():
     assert oh.subdivisionCode == subdivisionCode
+
 
 def test_url():
     url = "https://openholidaysapi.org/swagger/v1/swagger.json"
     assert oh.url("swagger/v1/swagger.json") == url
 
+
 def test_title():
     assert isinstance(oh.title, str)
+
 
 def test_description():
     assert isinstance(oh.description, str)
 
+
 def test_version():
     assert isinstance(oh.version, str)
 
+
 def test_isHoliday_true():
-    assert oh.isHoliday(startDate) == True
+    assert oh.isHoliday(startDate) is True
+
 
 def test_isHoliday():
-    assert oh.isHoliday(endDate) == False
+    assert oh.isHoliday(endDate) is False
+
 
 def test_publicHolidays():
     r = oh.publicHolidays(startDate, endDate)
     assert r[0]['startDate'] == startDate
     assert r[0]['endDate'] == startDate
     assert r[0]['type'] == "Public"
-    assert r[0]['nationwide'] == True
+    assert r[0]['nationwide'] is True
+
 
 def test_publicHolidaysByDate():
     r = oh.publicHolidaysByDate(startDate, languageIsoCode)
     assert r[0]['type'] == "Public"
 
+
 def test_schoolHolidays():
     r = oh.schoolHolidays(startDate, endDate)
     assert r[0]['type'] == "School"
+
 
 def test_schoolHolidaysByDate():
     r = oh.schoolHolidaysByDate(startDate, languageIsoCode)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,1 +1,17 @@
 # content of test_utils.py
+from school_bell import utils
+from datetime import date
+
+
+def test_init_logger():
+    assert isinstance(utils.init_logger(), utils.logging.Logger)
+
+
+def test_system_call():
+    assert utils.system_call(['echo', 'Hello, World']) is True
+
+
+def test_today_is_holiday():
+    today = f"{date.today()}"
+    oh = utils.OpenHolidays('BE', 'NL', 'NL-BE')
+    assert utils.today_is_holiday('NL-BE') == oh.isHoliday(today)
