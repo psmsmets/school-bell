d347866ec724a203dfaf84d9a109d44cf491bbd7
Pieter Smets <mail@pietersmets.be>
2026-09-01 22:55:48 +0200

    Make OpenHolidays startup fail-safe

--- tests/test_openholidays.py
+++ tests/test_openholidays.py
@@ -1,5 +1,9 @@
 # content of test_openholidays.py
 from datetime import date
+import pytest
+import requests
+
+import school_bell.openholidays as openholidays_module
 from school_bell.openholidays import OpenHolidays
 from school_bell.utils import to_date
 
@@ -12,6 +16,82 @@ startDate = to_date(date.today().strftime("%Y-01-01"))
 endDate = to_date(date.today().strftime("%Y-01-10"))
 
 oh = OpenHolidays(countryIsoCode, languageIsoCode, subdivisionCode, groupCode)
+
+
+class FakeResponse:
+    def __init__(self, data=None, status_error=None):
+        self.data = data
+        self.status_error = status_error
+        self.json_called = False
+
+    def raise_for_status(self):
+        if self.status_error:
+            raise self.status_error
+
+    def json(self):
+        self.json_called = True
+        return self.data
+
+
+def test_initialization_does_not_make_request(monkeypatch):
+    def unexpected_request(*args, **kwargs):
+        pytest.fail('OpenHolidays initialization made an HTTP request')
+
+    monkeypatch.setattr(openholidays_module.requests, 'get', unexpected_request)
+    OpenHolidays(countryIsoCode, languageIsoCode, groupCode=groupCode)
+
+
+def test_request_uses_default_timeout(monkeypatch):
+    calls = []
+
+    def get(*args, **kwargs):
+        calls.append((args, kwargs))
+        return FakeResponse([])
+
+    monkeypatch.setattr(openholidays_module.requests, 'get', get)
+    OpenHolidays(countryIsoCode).countries()
+
+    assert calls[0][1]['timeout'] == OpenHolidays.DEFAULT_TIMEOUT
+
+
+def test_request_timeout_can_be_overridden(monkeypatch):
+    calls = []
+
+    def get(*args, **kwargs):
+        calls.append((args, kwargs))
+        return FakeResponse([])
+
+    monkeypatch.setattr(openholidays_module.requests, 'get', get)
+    OpenHolidays(countryIsoCode).countries(timeout=2)
+
+    assert calls[0][1]['timeout'] == 2
+
+
+def test_none_timeout_uses_default(monkeypatch):
+    calls = []
+
+    def get(*args, **kwargs):
+        calls.append((args, kwargs))
+        return FakeResponse([])
+
+    monkeypatch.setattr(openholidays_module.requests, 'get', get)
+    OpenHolidays(countryIsoCode).countries(timeout=None)
+
+    assert calls[0][1]['timeout'] == OpenHolidays.DEFAULT_TIMEOUT
+
+
+def test_http_status_is_checked_before_json_is_parsed(monkeypatch):
+    response = FakeResponse(
+        [], requests.exceptions.HTTPError('request failed')
+    )
+    monkeypatch.setattr(
+        openholidays_module.requests, 'get', lambda *args, **kwargs: response
+    )
+
+    with pytest.raises(requests.exceptions.HTTPError):
+        OpenHolidays(countryIsoCode).countries()
+
+    assert response.json_called is False
 
 
 def test_countryIsoCode():
